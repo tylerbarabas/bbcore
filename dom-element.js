a1e01fc6bb4fc80397d50009337fee2ce2075838
Tyler Barabas <tylerbarabas@gmail.com>
2018-09-19 18:53:08 -0700

    clean up

--- dom-element.js
+++ dom-element.js
@@ -36,6 +36,36 @@ export default class DomElement {
         }
     }
 
+    handWrite( txt = "This is working!" ) {
+        let canvas = document.createElement('CANVAS');
+        this.dom.appendChild(canvas);
+        let ctx = canvas.getContext('2d');
+
+        let dashLen = 220, dashOffset = dashLen, speed = 3,
+            x = 30, i = 0;
+
+        ctx.font = "50px Comic Sans MS, cursive, TSCu_Comic, sans-serif"; 
+        ctx.lineWidth = 5; ctx.lineJoin = "round"; ctx.globalAlpha = 2/3;
+        ctx.strokeStyle = ctx.fillStyle = "#1f2f90";
+
+        (function loop() {
+          ctx.clearRect(x, 0, 60, 150);
+          ctx.setLineDash([dashLen - dashOffset, dashOffset - speed]); // create a long dash mask
+          dashOffset -= speed;                                         // reduce dash length
+          ctx.strokeText(txt[i], x, 90);                               // stroke letter
+
+          if (dashOffset > 0) requestAnimationFrame(loop);             // animate
+          else {
+            ctx.fillText(txt[i], x, 90);                               // fill final letter
+            dashOffset = dashLen;                                      // prep next char
+            x += ctx.measureText(txt[i++]).width + ctx.lineWidth * Math.random();
+            ctx.setTransform(1, 0, 0, 1, 0, 3 * Math.random());        // random y-delta
+            ctx.rotate(Math.random() * 0.005);                         // random rotation
+            if (i < txt.length) requestAnimationFrame(loop);
+          }
+        })();
+    }
+
     flashyflash(){
         this.style('filter', 'invert(100%)');
         var i = 0;
